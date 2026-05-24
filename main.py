3880c0e6f56d45c6faf844f9df30fd0cc13edf3b
JuanDiamond <jgonzalezzu@unal.edu.co>
2026-05-23 20:36:57 -0500

    prueba

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 from juego import Ahorcado
 juego = Ahorcado()
 juego.cargar_palabras("series", 1)
-print(juego.palabra)
+print(juego.palabra) #prueba de que si sirva la función y la muestre en consola
