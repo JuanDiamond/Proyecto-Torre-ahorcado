c4d0c7d87257382bf00cf2746a4937387c2a2a93
Salomon Jarro <chovengo2018@gmail.com>
2026-05-28 11:34:01 -0500

    se modifico el main y se adapto para añadir un pequeño apartado grafico y la nueva funcionalidad de selección de temas añadiendo que funcionase con la clase de juego anterormente programadas, donde se le da en el argumento de categoria el valor del selector de la misma . funciona perfecto

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
 from juego import Ahorcado
-juego = Ahorcado()
-juego.cargar_palabras("series", 1)
-print(juego.palabra) #prueba de que si sirva la función y la muestre en consola
+from MenuSeleccionTema import SelectorTema
+
+seleccion = 0
+
+if seleccion == 0:
+
+    print("I---- bienvenido a la torre del ahorcado ----I")
+    print(" ")
+    seleccion = int(input("            escriba 1 para empezar: "))
+    print(" ")
+
+if seleccion == 1:
+
+    selector = SelectorTema()
+    categoria = selector.SeleccionTematicas()
+
+    juego = Ahorcado()
+    juego.cargar_palabras(categoria, 1)
+    print(juego.palabra) #prueba de que si sirva la función y la muestre en consola
+
+
+
